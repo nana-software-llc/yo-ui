34421ee5935642b39bf9f3418ecd41dbb2a77e7e
Mi <thangkt@fpt.com>
2025-07-09 10:32:00 +0700

    feat: init storybok

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/yo-ui',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   trailingSlash: true,
   images: {
     unoptimized: true,
